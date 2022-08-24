010b36c0e830f455efb9c76c6933756591a0ff25
Sankla Rahul <raska@vestas.com>
2022-08-24 11:50:03 +0000

    tms test

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -12,6 +12,7 @@ sap.ui.define([
         return Controller.extend("project1.controller.View1", {
             formatter:formatter,
             onInit: function () {
+                
             }
         });
     });
